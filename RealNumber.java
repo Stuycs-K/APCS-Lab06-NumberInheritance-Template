7cdc6ad4735d90f8d25b313f5ab484a87d2af683
konstantinnovation <konstans@stuy.edu>
2022-11-07 12:18:16 -0500

    Create RealNumber.java

---
+++ RealNumber.java
@@ -0,0 +1,52 @@
+public class RealNumber{
+  private double value;
+
+  public RealNumber(double v){
+    value = v;
+  }
+
+  public double getValue(){
+    return value;
+  }
+
+  public String toString(){
+    return ""+value;
+  }
+  
+  /**
+  *@return the sum of this and the other
+  *@param other - ANY RealNumber, including a RationalNumber or other subclasses of RealNumber (that aren't written yet)
+  */
+  public double add(RealNumber other){
+    //COMPLETE THIS METHOD
+    return 0;
+  }
+
+  /**
+  *@return the product of this and the other
+  *@param other - ANY RealNumber, including a RationalNumber or other subclasses of RealNumber (that aren't written yet)
+  */
+  public double multiply(RealNumber other){
+    //COMPLETE THIS METHOD
+    return 0;
+  }
+
+  /**
+  *@return the this divided by the other
+  */
+  public double divide(RealNumber other){
+    //COMPLETE THIS METHOD
+    return 0;
+  }
+
+  /**
+  *@return the this minus the other
+  */
+  public double subtract(RealNumber other){
+    //COMPLETE THIS METHOD
+    return 0;
+  }
+
+
+
+}
