8792a32498667afb6ba0c9adb7a9f9ae08f5bfd9
Samuel Konstantinovich <konstans@gmail.com>
2024-11-08 08:42:33 -0500

    oops

--- RationalNumber.java
+++ RationalNumber.java
@@ -1,21 +1,24 @@
 public class RationalNumber extends RealNumber
 {
+  //Hint: ONLY touch instance variables directly in your accessor methods/constructor.
   private int numerator, denominator;
 
   /**Initialize the RationalNumber with the provided values
   *  When the denominator is negative, negate both numerator and denominator.
   *  This handles all cases including if both are negative.
-  *  When the denominator parameter is 0, make the fraction 0/1 regardless of the 
-  *  numerator. This is not mathematically correct, but we can decide on the behavior to 
+  *  When the denominator parameter is 0, make the fraction 0/1 regardless of the
+  *  numerator. This is not mathematically correct, but we can decide on the behavior to
   *  avoid issues.
-  *  All RationalNumbers must reduce() upon construction.
+  *******************************************************
+  *  All RationalNumbers must automatically reduce() upon construction.
+  *******************************************************
   *@param nume the numerator
   *@param deno the denominator
   */
   public RationalNumber(int nume, int deno){
     super(0.0);//this value is ignored!
   }
-  
+
   /**
   *@return the numerator
   */
@@ -33,7 +36,6 @@ public class RationalNumber extends RealNumber
   //NOTE! For two reasons, the @Override annotation is useful.
   //1. If the annotated method does not actually override anything, the compiler issues a warning.
   //2. It can help to make the source code more readable.
-  
   @Override
   public double getValue(){
     //COMPLETE THIS METHOD
@@ -48,19 +50,23 @@ public class RationalNumber extends RealNumber
     //COMPLETE THIS METHOD
     return null;
   }
-  
+
   /**
-  *@return true when the RationalNumbers have the same numerators and denominators, false otherwise.
-  *note, this is NOT an override. If you use equals on a RealNumber that is the other method.
+  *@return true if and only if the RationalNumbers are both 0,
+  *        or they have the same numerators and denominators,
+  *return false otherwise.
+  *note, this is NOT an override. If you use equals on a RealNumber
+  *      that is the other method.
   */
-  public boolean equals(RationalNumber other){
+public boolean equals(RationalNumber other){
     //COMPLETE THIS METHOD
     return false;
   }
 
 
   /**
-  *@return the value expressed as "3/4" or "8/3", however 4/1 should be "4"
+  *@return the value expressed as "3/4" or "8/3",
+  *however 4/1 should be "4", and 0/1 should be 0.
   */
   @Override
   public String toString(){
@@ -74,9 +80,11 @@ public class RationalNumber extends RealNumber
   *@return the value of the GCD
   */
   public static int gcd(int a, int b){
-    //COMPLETE THIS METHOD
-    /*use euclids method or a better one*/
-    // http://sites.math.rutgers.edu/~greenfie/gs2004/euclid.html
+    /*COMPLETE THIS METHOD
+    Easy: You can just loop over all the values and return the largest.
+    *Clever: use euclids method if you want to be efficient
+    * http://sites.math.rutgers.edu/~greenfie/gs2004/euclid.html
+    */
     return 0;
   }
 
@@ -88,10 +96,10 @@ public class RationalNumber extends RealNumber
   public void reduce(){
 
   }
-  
+
   /******************new math operations Return a new RationalNumber!!!!****************/
   //Note: RealNumber methods are inherited, these are NOT override, they are different methods.
-  
+
   /**
   *@return a new RationalNumber that is the product of this and the other
   */
--- RealNumber.java
+++ RealNumber.java
@@ -1,4 +1,5 @@
 public class RealNumber{
+  //Hint: ONLY touch instance variables directly in your accessor methods/constructor.
   private double value;
 
   public RealNumber(double v){
@@ -9,20 +10,32 @@ public class RealNumber{
     return value;
   }
 
+  @Override
   public String toString(){
     return ""+value;
   }
-  
+
+
+  /**
+  *@return true if and only if the other parameter
+    has the same value as this.
+    hint: you should always use accessor methods.
+  */
+  public boolean equals(RealNumber other){
+    //complete this method
+    return true;
+  }
+
   /**
   *@return 0 when other is the same as this,
-    a positive value when this is greater than other, 
+    a positive value when this is greater than other,
     or a negative value when this is less than other
   */
   public int compareTo(RealNumber other){
     //complete this method
     return 0;
   }
-  
+
   /**
   *@return the sum of this and the other
   *@param other - ANY RealNumber, including a RationalNumber or other subclasses of RealNumber (that aren't written yet)
